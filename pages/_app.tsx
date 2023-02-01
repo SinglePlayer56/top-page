aff7c56010bd4d80a3d2211c0b1238f18805dc97
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-02-01 21:20:02 +0500

    added meta and og tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import Head from "next/head";
 
-export default function App({Component, pageProps}: AppProps): JSX.Element {
+export default function App({Component, pageProps, router}: AppProps): JSX.Element {
     return (
         <>
             <Head>
                 <title>My top App</title>
                 <link rel="icon" href="/favicon.ico"/>
-                <meta name={'description'} content={'My app top application'}/>
+                <meta property={'og:url'} content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
+                <meta property={'og:locale'} content={'ru_RU'}/>
             </Head>
             <Component {...pageProps} />
         </>
